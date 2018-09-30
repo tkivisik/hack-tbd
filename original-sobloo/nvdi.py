893c2479b61f029004adf17430736e281262afb8
Taavi Kivisik <taavi.kivisik@gmail.com>
2018-09-30 08:58:02 +0000

    Minimally working download, slicing

--- original-sobloo/nvdi.py
+++ original-sobloo/nvdi.py
@@ -6,30 +6,45 @@ import ipywidgets as ipyw
 #import eodag as dag2
 from eodag.api.core import EODataAccessGateway
 
-base_dir = '/sources/TutoDownload'
+base_dir = '/home/sobloo/hack-tbd/original-sobloo'
 conf_file = base_dir + "/eodagconf.yml"
 dag = EODataAccessGateway(user_conf_file_path = conf_file)
 product_type = 'S2_MSI_L1C'
 extent = {
-  'lonmin': 1.306000,
-  'lonmax': 1.551819,
-  'latmin': 43.527642,
-  'latmax': 43.662905
+'lonmin': 18.294550,
+'lonmax': 18.361420,
+'latmin': 59.397867,
+'latmax': 59.414504
 }
+#  'lonmin': 1.306000,
+#  'lonmax': 1.551819,
+#  'latmin': 43.527642,
+#  'latmax': 43.662905
+#}
+
 dag.set_preferred_provider(provider='airbus-ds')
 #prodTypeList = dag.list_product_types('airbus-ds')
 #print(prodTypeList)
 
-#products = dag.search(product_type,startTimeFromAscendingNode='2018-05-01',completionTimeFromAscendingNode=date.today().isoformat(),geometry=extent,cloudCover=1)
-products = dag.search(product_type)
-print(products)
+products = dag.search(product_type,startTimeFromAscendingNode='2016-01-17',completionTimeFromAscendingNode='2017-12-20',geometry=extent,cloudCover=1)
+#products = dag.search(product_type)
+for i in range(len(products)):
+    print('{} : {}'.format(i, products[i]))
+#print(products)
 product = products[0]
+xx, yy = product.as_dict()['geometry']['coordinates'][0][4]
 
-VIR = product.get_data(crs='epsg:4326', resolution=0.0001, band='B04', extent=(1.435905, 43.586857, 1.458907, 43.603827))
-NIR = product.get_data(crs='epsg:4326', resolution=0.0001, band='B08', extent=(1.435905, 43.586857, 1.458907, 43.603827))
+const = 0.05
+longmin = xx-const
+latmin = yy-const
+longmax = xx+const
+latmax = yy+const
+print("{}, {}, {}, {}".format(longmin, longmax, latmin, latmax))
+
+VIR = product.get_data(crs='epsg:4326', resolution=0.0001, band='B04', extent=(longmin, latmin, longmax, latmax))
+NIR = product.get_data(crs='epsg:4326', resolution=0.0001, band='B08', extent=(longmin, latmin, longmax, latmax))
 NDVI = (NIR - VIR * 1.) / (NIR + VIR)
 
 plt.imshow(NDVI, cmap='RdYlGn', aspect='auto')
 plt.savefig('%s/img/ndvi_toulouse.png' % base_dir)
-
 
